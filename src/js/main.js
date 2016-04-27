3bd1ef08581cadf53ba362d8796d2ddb76218ee3
Denys Kostenko <denys_kostenko@epam.com>
2016-04-27 20:04:51 +0300

    finish task

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Denys_Kostenko on 4/27/2016.
+ */
+window.jQuery = require('jquery');
+require('bootstrap');
+var whitespaceDot = require('whitespace-dot');
+
+jQuery('.btn').on('click', function () {
+  console.log(whitespaceDot.replaceWhitespaces('CDP hometask 2'))
+});
